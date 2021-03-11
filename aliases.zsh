4c4a688e4060dc8a38d553329963020747079d86
Aaron <kingjojo3@gmail.com>
2021-03-11 13:03:15 -0500

    Added aliases

---
+++ aliases.zsh
@@ -0,0 +1,9 @@
+alias ls=exa 
+# alias ssh=xxh
+alias vi=vim 
+alias sudo='sudo '
+alias feck='sudo $(fc -ln -1)' 
+alias gvconnect='sudo openconnect --protocol=pulse https://vpn.student.gvsu.edu'
+alias clang='clang -Wall' 
+alias gcc='gcc -Wall'
+alias python=python3
