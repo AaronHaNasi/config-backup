310416daf700583e9087fd8deb52df9652d7c5a5
Aaron <kingjojo3@gmail.com>
2021-03-18 16:41:45 -0400

    Backing up new aliases

--- aliases.zsh
+++ aliases.zsh
@@ -6,4 +6,6 @@ alias feck='sudo $(fc -ln -1)'
 alias gvconnect='sudo openconnect --protocol=pulse https://vpn.student.gvsu.edu'
 alias clang='clang -Wall' 
 alias gcc='gcc -Wall'
+alias g++='g++ -Wall'
 alias python=python3
+alias less='less -N'
